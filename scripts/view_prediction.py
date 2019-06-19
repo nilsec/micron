054869db81cd54a8f6d699ed354fdefa41fb676c
nilsec <nils.eckstein@googlemail.com>
2019-06-19 15:05:37 -0400

    Add support for viewing selected_only graph

--- scripts/view_prediction.py
+++ scripts/view_prediction.py
@@ -8,8 +8,9 @@ from db_scripts import get_graph
 from funlib.show.neuroglancer import add_layer, ScalePyramid
 
 
-predict_config = "/groups/funke/home/ecksteinn/Projects/microtubules/micron_experiments/cremi/02_predict/setup_2/predict_config_0.ini"
+predict_config = "/groups/funke/home/ecksteinn/Projects/microtubules/micron_experiments/cremi/02_predict/setup_2/predict_config_template.ini"
 dsets = ["volumes/soft_mask"]
+selected_only=True
 
 config = configparser.ConfigParser()
 config.read(predict_config)
@@ -37,27 +38,18 @@ for dset in dsets:
 nodes, edges = get_graph(db_host,
                          db_name,
                          roi_offset,
-                         roi_size)
-
-if nodes:
-    maxima = []
-    maxima_dict = {}
-    for z,y,x,node_id in zip(nodes["z"], nodes["y"], nodes["x"], nodes["id"]):
-        maxima_dict[node_id] = (x,y,z)
-        maxima.append(neuroglancer.EllipsoidAnnotation(center=(x,y,z+1), 
-                                                       radii=(tuple([10] * 3)),
-                                                       id=node_id,
-                                                       segments=None
-                                                       )
-                     )
-
+                         roi_size,
+                         selected_only)
 if edges:
+    nodes_in_edges = []
     k = 0
     edge_connectors = []
-    for u, v in zip(edges["u"], edges["v"]):
+    for u, v, selected, solved in edges:
         try:
-            pos_u = maxima_dict[u]
-            pos_v = maxima_dict[v]
+            pos_u = (nodes[u][2], nodes[u][1], nodes[u][0])
+            pos_v = (nodes[v][2], nodes[v][1], nodes[v][0])
+            nodes_in_edges.append(u)
+            nodes_in_edges.append(v)
 
             edge_connectors.append(neuroglancer.LineAnnotation(point_a=pos_u,
                                                                point_b=pos_v,
@@ -67,6 +59,33 @@ if edges:
             k += 1
         except KeyError:
             pass
+
+if nodes:
+    maxima = []
+    maxima_dict = {}
+    if not selected_only:
+        for node_id,z,y,x,selected,solved in nodes:
+            maxima_dict[node_id] = (x,y,z)
+            maxima.append(neuroglancer.EllipsoidAnnotation(center=(x,y,z+1), 
+                                                           radii=(tuple([10] * 3)),
+                                                           id=node_id,
+                                                           segments=None
+                                                           )
+                         )
+
+    else:
+        for node_id in nodes_in_edges:
+            x = nodes[node_id][2]
+            y = nodes[node_id][1]
+            z = nodes[node_id][0]
+            maxima_dict[node_id] = (x,y,z)
+            maxima.append(neuroglancer.EllipsoidAnnotation(center=(x,y,z+1), 
+                                                           radii=(tuple([10] * 3)),
+                                                           id=node_id,
+                                                           segments=None
+                                                           )
+                         )
+
 
 viewer = neuroglancer.Viewer()
 with viewer.txn() as s:
