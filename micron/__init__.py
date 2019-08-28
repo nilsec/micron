6062fa336fbf3532d92d31563f14d922c5749060
nilsec <nils.eckstein@googlemail.com>
2019-08-28 15:29:26 -0400

    Rename to avoid package name clash

--- micron/__init__.py
+++ micron/__init__.py
@@ -1,5 +1,5 @@
 from .read_config import read_predict_config, read_worker_config, read_data_config, read_solve_config, read_graph_config
 from . import graph
 from . import network
-from . import gunpowder
+from . import gp
 from . import solve
