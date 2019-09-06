9b987f10329101b1c48face13059fb658dccb1cd
nilsec <nils.eckstein@googlemail.com>
2019-09-06 16:22:44 -0400

    Add read train config

--- micron/__init__.py
+++ micron/__init__.py
@@ -1,4 +1,4 @@
-from .read_config import read_predict_config, read_worker_config, read_data_config, read_solve_config, read_graph_config
+from .read_config import read_predict_config, read_worker_config, read_data_config, read_solve_config, read_graph_config, read_train_config
 from . import graph
 from . import network
 from . import gp
