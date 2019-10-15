ef9cfe96a7f282d998ed98c978c3a3f523775951
nilsec <nils.eckstein@googlemail.com>
2019-10-15 18:31:55 -0400

    Make config read more general

--- micron/solve/solve.py
+++ micron/solve/solve.py
@@ -5,17 +5,20 @@ import logging
 import scipy.interpolate.interpnd # Workaround for cython __reduce_cython__ error
 from micron import read_predict_config, read_worker_config, read_data_config, read_graph_config, read_solve_config
 
-predict_config = read_predict_config("predict_config.ini")
-worker_config = read_worker_config("worker_config.ini")
-data_config = read_data_config("data_config.ini")
-graph_config = read_graph_config("graph_config.ini")
-solve_config = read_solve_config("solve_config.ini")
+directory = os.path.dirname(os.path.abspath(__file__))
+predict_config_path = os.path.join(directory, "predict_config.ini") 
+worker_config_path = os.path.join(directory, "worker_config.ini")
+data_config_path = os.path.join(directory,"data_config.ini")
+graph_config_path = os.path.join(directory, "graph_config.ini")
+solve_config_path = os.path.join(directory, "solve_config.ini")
+
+solve_config = read_solve_config(solve_config_path)
 
 base_cmd = "python {} {} {} {} {} {}".format(solve_config["daisy_solve"],
-                                             os.path.abspath("predict_config.ini"),
-                                             os.path.abspath("worker_config.ini"),
-                                             os.path.abspath("data_config.ini"),
-                                             os.path.abspath("graph_config.ini"),
-                                             os.path.abspath("solve_config.ini"))
+                                             predict_config_path,
+                                             worker_config_path,
+                                             data_config_path,
+                                             graph_config_path,
+                                             solve_config_path)
 
 check_call(base_cmd, shell=True)
