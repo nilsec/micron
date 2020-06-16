6fbacdbc5c2eaced47aa3eaf80ad518e33d811b9
nilsec <nils.eckstein@googlemail.com>
2020-06-16 06:50:51 -0400

    Add nx to nml converter

--- micron/scripts/db_scripts.py
+++ micron/scripts/db_scripts.py
@@ -13,6 +13,64 @@ def build_attributes(xml_elem, attributes):
             xml_elem.setAttribute(attr[0], str(attr[1].encode('ascii', 'replace')))
     return xml_elem
 
+def nx_to_nml(nodes,
+              edges,
+              output_file,
+              voxel_size=(40,4,4)):
+
+
+
+    #nodes = {v[0]: (v[1]["z"], v[1]["y"], v[1]["x"]) for v in nx_graph.nodes(data=True)}
+    #edges = [(e[0], e[1]) for e in nx_graph.edges()]
+
+    doc = minidom.Document()
+    annotations_elem = doc.createElement("things")
+    doc.appendChild(annotations_elem)
+
+    annotation_elem = doc.createElement("thing")
+    build_attributes(annotation_elem, [["id", 3]])
+
+    nodes_elem = doc.createElement("nodes")
+    edges_elem = doc.createElement("edges")
+
+    for node_id, position in nodes.items():
+        node_elem = doc.createElement("node")
+        #position = (np.array(position) - np.array(roi_offset))/voxel_size 
+        position = np.array(position)/voxel_size
+        position = np.rint(position).astype(int) 
+        identifier = node_id
+
+        build_attributes(node_elem, [["x", position[2]],
+                                     ["y", position[1]],
+                                     ["z", position[0]],
+                                     ["id", node_id]
+                                    ])
+
+        nodes_elem.appendChild(node_elem)
+
+    for e in edges:
+        source_id = e[0]
+        target_id = e[1]
+
+        edge_elem = doc.createElement("edge")
+
+        build_attributes(edge_elem, [["source", source_id],
+                                     ["target", target_id]
+                                    ])
+
+        edges_elem.appendChild(edge_elem)
+
+    annotation_elem.appendChild(nodes_elem)
+    annotation_elem.appendChild(edges_elem)
+
+    annotations_elem.appendChild(annotation_elem)
+
+    doc = doc.toprettyxml()
+
+    with open(output_file, "w+") as f:
+        f.write(doc)
+
+
 
 def graph_to_nml(output_file,
                  db_host, 
@@ -34,7 +92,6 @@ def graph_to_nml(output_file,
                               solved_attr,
                               edge_collection)
 
-    
     doc = minidom.Document()
     annotations_elem = doc.createElement("things")
     doc.appendChild(annotations_elem)
@@ -121,6 +178,6 @@ def label_connected_components(db_host,
     graph_provider = MongoDbGraphProvider(db_name, db_host, directed=False, position_attribute=['z', 'y', 'x'], edges_collection=edge_collection)
     graph = graph_provider.get_graph(roi, nodes_filter={selected_attr: True}, edges_filter={selected_attr: True})
 
-    find_connected_components(graph, node_component_attribute=label_attribute, return_lut=False)
+    lut = find_connected_components(graph, node_component_attribute=label_attribute, return_lut=True)
 
-    return graph
+    return graph, lut
